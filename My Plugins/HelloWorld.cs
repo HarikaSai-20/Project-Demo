604c7e033070baf8bcac83d03583a41601d8c3eb
Harika <harika8476@gmail.com>
2026-02-23 12:49:14 +0530

    hello world plugin added

---
+++ My Plugins/HelloWorld.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk;
+using System.Runtime.Remoting.Contexts;
+
+namespace MyPlugins
+{
+    public class HelloWorld : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                try
+                {
+                    String firstname = String.Empty;
+
+                    if (entity.Attributes.Contains("firstname"))
+                    {
+                        firstname = entity.Attributes["firstname"].ToString();
+                    }
+                    String lastname = entity.Attributes["lasname"].ToString();
+
+                    entity.Attributes.Add("description", "Hello World " + firstname + lastname);
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("Plugin execution is Invalid", ex);
+                }
+                catch (Exception ex)
+                {
+                    tracingService.Trace("My Plugin:{0}", ex.ToString());
+                    throw;
+                }
+
+            }
+        }
+    }
+}
